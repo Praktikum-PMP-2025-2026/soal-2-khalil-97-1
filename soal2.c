82e8cdd60838aed3e0cae02f2f6d14b3aaba496e
khalil-97 <khalilalfaiz79@gmail.com>
2026-04-29 09:56:07 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+const int a = 1;
 typedef struct {
     char nama[101];
     int bobot;
@@ -13,8 +13,24 @@ int perbandingan(const void *a, const void *b) {
     artefak *y = (artefak *)b;
     return strcmp(x->nama, y->nama);
 }
+int lexKecil(unsigned long long a, unsigned long long b, int n) {
+    int ia = 0, ib = 0;
 
-const int a = 1;
+    while (1) {
+
+        if (ia == n && ib == n) return 0;
+        if (ia == n) return 1;
+        if (ib == n) return 0;
+
+        if (ia < ib) return 1;
+        if (ia > ib) return 0;
+
+        ia++;
+        ib++;
+    }
+}
+
+
 int main() {
     int N, W;
     scanf("%d %d", &N, &W);
@@ -29,9 +45,9 @@ int main() {
 
     int bestnilai = -1;
     int bestbobot = 0;
-    int bestMask = 0;
+    unsigned long long bestMask = 0;
 
-    int total = 1;
+    int total = a << N;
 
     for (int mask = 0; mask < total; mask++) {
         int berat = 0;
@@ -46,8 +62,9 @@ int main() {
 
         if (berat <= W) {
             if (nilai > bestnilai ||
-                (nilai == bestnilai && berat < bestbobot)
-               ) {
+                (nilai == bestnilai && berat < bestbobot)||
+                (nilai == bestnilai && berat == bestbobot && lexKecil(mask, bestMask, N)
+               )) {
                 bestnilai = nilai;
                 bestbobot = berat;
                 bestMask = mask;
